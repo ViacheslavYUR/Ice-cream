eaa3aed1fa7af2407122503d045357d73f416469
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2022-09-05 01:38:49 +0300

    add read more & our location modal
    read more without video

---
+++ src/js/read-modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[read-modal-open]"),
+    closeModalBtn: document.querySelector("[read-modal-close]"),
+    modal: document.querySelector("[read-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop--hidden");
+  }
+})();
