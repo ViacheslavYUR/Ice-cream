203a1ebd2659d06aecb408548cae1710bcd0923e
ViacheslavYUR <108462275+ViacheslavYUR@users.noreply.github.com>
2022-09-04 23:01:08 +0000

    Deploying to gh-pages from @ ViacheslavYUR/Ice-cream@45a3e67c8be64914917e91105f344850a469dcf2 🚀

--- index.a05b1361.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){document.body.classList.toggle("modal-open"),e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=index.a05b1361.js.map
