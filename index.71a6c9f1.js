aedc69c9ba70d5995a22dd755751603e21c1c227
ViacheslavYUR <108462275+ViacheslavYUR@users.noreply.github.com>
2022-09-04 22:47:32 +0000

    Deploying to gh-pages from @ ViacheslavYUR/Ice-cream@fdaee0c79f73bca05812c35c9f4de90cffbc21b6 🚀

---
+++ index.71a6c9f1.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[location-modal-open]"),closeModalBtn:document.querySelector("[location-modal-close]"),modal:document.querySelector("[location-modal]")};function e(){o.modal.classList.toggle("backdrop--hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.71a6c9f1.js.map
---
+++ index.c680b785.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[read-modal-open]"),closeModalBtn:document.querySelector("[read-modal-close]"),modal:document.querySelector("[read-modal]")};function o(){e.modal.classList.toggle("backdrop--hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.c680b785.js.map
