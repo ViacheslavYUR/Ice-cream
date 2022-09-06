77e56e00e937b56c1a009bcaf39cf82ae40b60aa
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-09-06 20:42:30 +0300

    1
    test-not working

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    menuWrap: document.querySelector('[data-backdrop]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
@@ -11,5 +12,6 @@
   function toggleModal() {
     document.body.classList.toggle("modal-open");
     refs.menu.classList.toggle('is-open');
+    refs.modal.classList.toggle("backdrop--hidden");
   }
 })();
