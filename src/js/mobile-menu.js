926b92a5250b7bc0a7c24ba00e4acdb030711a9d
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-09-25 16:52:02 +0300

    Update mobile-menu.js
    add function to leave mobile-menu by clicking refs mobile-menu.

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,7 +3,6 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-    // menuWrap: document.querySelector('[data-backdrop]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
@@ -12,15 +11,12 @@
   function toggleModal() {
     document.body.classList.toggle('modal-open');
     refs.menu.classList.toggle('is-open');
-    // refs.modal.classList.toggle("backdrop--hidden");
   }
-
-  function closeMenu() {
-    const mobileLinks = Document.getElementsByClassName('js-mobile-menu__link');
-    for (const mobileLink of mobileLinks) {
-      mobileLink.addEventListener('click', function () {
-        refs.menu.classList.remove('is-open');
-      });
+  const btnRefs = document.querySelectorAll('.js-mobile-menu__link');
+  for (const btnRef of btnRefs) {
+    btnRef.addEventListener('click', closeMobileMenu);
+    function closeMobileMenu() {
+      refs.menu.classList.remove('is-open');
     }
   }
 })();
