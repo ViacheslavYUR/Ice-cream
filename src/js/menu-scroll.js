b2d7a4ffffa1584983cda634bb148c222ee48c31
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2022-12-26 11:43:01 +0200

    improve ui scroll-button
    hide scroll-button, when it's approaching to the hader

--- src/js/menu-scroll.js
+++ src/js/menu-scroll.js
@@ -1,13 +1,27 @@
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-  smoothLink.addEventListener('click', (e) => {
-          e.preventDefault();
-          const id = smoothLink.getAttribute('href');
+const throttle = require('lodash.throttle');
 
-          document.querySelector(id).scrollIntoView({
-              behavior: 'smooth',
-              block: 'start',
-          });
-      });
-  
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+const scrollBtn = document.querySelector('.scroll-up-btn');
+
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', e => {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
 }
+
+scrollBtn.classList.add('js-hidden');
+window.addEventListener('scroll', throttle(onScroll, 500));
+
+function onScroll() {
+  if (window.scrollY > 400) {
+    scrollBtn.classList.remove('js-hidden');
+  } else if (window.scrollY <= 400) {
+    scrollBtn.classList.add('js-hidden');
+  }
+}
+
